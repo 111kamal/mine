68482bded727cbb9e1903351fceac8f21fd55a98
111kamal <krajzro486@gmail.com>
2026-02-07 18:40:43 +0530

    Add files via upload

---
+++ rajjj/script.js
@@ -0,0 +1,17 @@
+function unlock(){
+  let p = document.getElementById("pass").value;
+  if(p === "NEELKAMAL"){
+    window.location="home.html";
+  } else {
+    document.getElementById("msg").innerText =
+    "Ask your baby for the password 💕";
+  }
+}
+
+function go(page){
+  window.location.href = page;
+}
+
+function goBack(){
+  window.history.back();
+}
